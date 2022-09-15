c847047ac5fbf316ed5f4959fd2f33e52805fb39
ORLANDO CHOQUE AYMA <orlando_choque@users.noreply.github.com>
2022-09-14 23:16:50 -0400

    mejorado

--- App.js
+++ App.js
@@ -1,7 +1,90 @@
-import React, { Component } from "react";
-import { FlatList, StyleSheet, Switch, TouchableHighlight, View, Image } from "react-native";
-import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
 
+import  { Component } from "react";
+//import * as React from 'react';
+import { Button, FlatList, StyleSheet, Text, Switch, TouchableHighlight, 
+	View, Image, TouchableOpacity, PermissionsAndroid, Platform } from "react-native";
+// Import RNFetchBlob for the file download
+import RNFetchBlob from 'rn-fetch-blob';
+//import { Button } from 'react-native-paper';
+const REMOTE_IMAGE_PATH = 'https://usip.edu.bo/wp-content/uploads/2021/09/usip-marca.png';
+const REMOTE_IMAGE_PATH_X = 'https://usip.edu.bo/wp-content/uploads/2021/09/usip-marca.png';
+const checkPermission = async (archivo) => {
+    
+    // Function to check the platform
+    // If iOS then start downloading
+    // If Android then ask for permission
+	if (Platform.OS === 'ios') {
+      downloadImage(archivo);
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          {
+            title: 'Storage Permission Required',
+            message:
+              'App needs access to your storage to download Photos',
+          }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          // Once user grant the permission start downloading
+          console.log('Storage Permission Granted.');
+          downloadImage(archivo);
+        } else {
+          // If permission denied then show alert
+          alert('Storage Permission Not Granted');
+        }
+      } catch (err) {
+        // To handle permission related exception
+        console.warn(err);
+      }
+    }
+  };
+  const downloadImage = (archi) => {
+    // Main function to download the image
+    
+    // To add the time suffix in filename
+    let date = new Date();
+    // Image URL which we want to download
+    //let image_URL = REMOTE_IMAGE_PATH;    
+	let image_URL = archi;    
+    // Getting the extention of the file
+    let ext = getExtention(image_URL);
+    ext = '.' + ext[0];
+    // Get config and fs from RNFetchBlob
+    // config: To pass the downloading related options
+    // fs: Directory path where we want our image to download
+    const { config, fs } = RNFetchBlob;
+    let PictureDir = fs.dirs.PictureDir;
+    let options = {
+      fileCache: true,
+      addAndroidDownloads: {
+        // Related to the Android only
+        useDownloadManager: true,
+        notification: true,
+        path:
+          PictureDir +
+          '/image_' + 
+          Math.floor(date.getTime() + date.getSeconds() / 2) +
+          ext,
+        description: 'Image', 
+      },
+    };
+    config(options)
+      .fetch('GET', image_URL)
+      .then(res => {
+        console.log(image_URL);
+        // Showing alert after successful downloading
+        console.log('res -> ', JSON.stringify(res));
+        alert('Image Downloaded Successfully.');
+      });
+  };
+ 
+  const getExtention = filename => {
+    // To get the file extension
+    return /[.]/.exec(filename) ?
+             /[^.]+$/.exec(filename) : undefined;
+  };
+ 
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,7 +92,9 @@ class App extends Component {
       count: 0,
       data:[],
       loading: false,
-      url: 'https://randomuser.me/api?results=9',
+      //url: 'https://randomuser.me/api?results=6',
+	  //url: 'https://pokeapi.co/api/v2/pokemon',
+	  url: 'https://www.themealdb.com/api/json/v1/1/categories.php',
       prev: null,
       next: null,
       isEnabled: false,
@@ -25,7 +110,6 @@ class App extends Component {
     this.setState({isEnabled: !ise})
   };
 
-<<<<<<< HEAD
 
   getData = async() => {
     this.setState({loading: true});
@@ -34,7 +118,7 @@ class App extends Component {
    const prev = json.previous;
     const next = json.next;
     this.setState({
-      data: json.results,
+      data: json.categories,
       loading: false
     });
   }
@@ -42,43 +126,13 @@ class App extends Component {
 
     if(this.state.loading) {
       return (
-        <View style={styles.container}>
+        //<View style={styles.container}>
         <View style={[styles.countContainer]}>
           <Text style={[styles.countText]}>
-            Iniciando las Tarjeta
+          Cargando ...
           </Text>
-=======
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-           hola mundo orlando
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
->>>>>>> 7f1ba2bedfc512036a16beb5f5483391240331cd
         </View>
-      </View>
+      //</View>
       );
     }
 
@@ -86,40 +140,58 @@ class App extends Component {
     return (
       <View>
         <View style={styles.header}>
+
+		<Image
+        source={{
+          uri: REMOTE_IMAGE_PATH,
+        }}
+        style={{
+          width: '100%',
+          height: 100,
+          resizeMode: 'contain',
+          margin: 5
+        }}
+      />
         <Text> Horizontal</Text>
         <Switch
           trackColor={{ false: "#767577", true: "#81b0ff" }}
           thumbColor={this.state.isEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#114e3f"
+          ios_backgroundColor="#3e3e3e"
           onValueChange={ () => { 
             this.setState({isEnabled: !this.state.isEnabled})
           }}
           value={this.state.isEnabled}
-      
         />
         </View>
         <View>
         <View style={[styles.countContainer]}>
-          <Text>Escoje tu Tarjeta</Text>
+          <Text>Listado</Text>
           <FlatList
             horizontal={this.state.isEnabled ? true : false}
             animated={true}
             data={this.state.data}
             keyExtractor={(x,i)=> i}
             renderItem={({item})=>
-            <View style={styles.contenedor}>
-              <Text>{item.name.first}</Text>
-              <Text>{item.name.last}</Text>
-              <Image
+			<View style={styles.contenedor}>
+              <Text>{item.strCategory}</Text>
+			  <Image
                 style={styles.logo}
-                source={{uri: item.picture.large}}
-              />
-              <Text>{item.email}</Text>
-              <Button icon="camera" mode="contained" onPress={() => this.showModal(item.url)}>
-									Detalle
-							</Button>
-            </View>}
-          />
+                source={{uri: item.strCategoryThumb}}
+				/>
+				<TouchableOpacity
+        			style={styles.button}
+        			onPress={()=>{
+					    let REMOTE_IMAGE_PATH_X=	item.strCategoryThumb
+						checkPermission (REMOTE_IMAGE_PATH_X)
+					}
+				}>
+        			<Text style={styles.text}>
+          			{item.strCategoryThumb}
+        			</Text>
+      			</TouchableOpacity>
+              </View>
+     }
+       />
         </View>
       </View>
       </View>
@@ -148,16 +220,16 @@ const styles = StyleSheet.create({
   tinyLogo: {
     width: 50,
     height: 50,
+	alignContent:'center',
   },
   logo: {
-    width: 200,
-    height: 200,
-    alignItems: "center",
+    width: 150,
+    height: 150,
   },
   contenedor: {
     width: 300,
     height: 450,
-    backgroundColor: '#AA8829',
+    backgroundColor: '#AADE29',
     marginLeft: 5,
   },
   header: {
