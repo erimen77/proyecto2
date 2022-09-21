62f0e14cebdb8eea055cbf91ce2cfc1d379e2328
ORLANDO CHOQUE AYMA <orlando_choque@users.noreply.github.com>
2022-09-20 22:11:06 -0400

    or_20_09

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { ButoonTab } from './src/navigation/ButoonTab'
-
+// principal 
 const App = () => {
   return (
     <NavigationContainer>
